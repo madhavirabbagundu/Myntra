8f285e71a3470093c1bf426c7ecdb4dec2d945be
Madhavi Rabbagundu <madhavirabbagundu@gmail.com>
2023-10-13 22:19:29 +0530

    completed home page and working on login page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
+import wear from '../images/wear.png'
+import img1 from '../images/img1.png'
+import img2 from '../images/img2.png'
+import img3 from '../images/img3.png'
+
 
 function Home(){
     return(
         <>
         <div className='home'>
-            <img className = "item" src = "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2023/10/4/c697c000-3cf1-48f8-8130-c15ff23306401696438189573-FLAT-300-Off-on-1st-Purchase-Strip--2-.jpg"/>
-        </div>
+            <img className = "item" src = "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2023/10/4/c697c000-3cf1-48f8-8130-c15ff23306401696438189573-FLAT-300-Off-on-1st-Purchase-Strip--2-.jpg" alt = ""/>
+           <div className='item1'>
+            <img className="item2" src = "https://assets.myntassets.com/f_webp,w_490,c_limit,fl_progressive,dpr_2.0/assets/images/2023/10/5/b23170c2-fbef-47ee-9e77-d78e8e05853e1696485422051-Desktop-Main-Banner_01.jpg" alt = ""/>
+            <img className = "item2" src = "https://assets.myntassets.com/f_webp,w_490,c_limit,fl_progressive,dpr_2.0/assets/images/2023/10/5/a2154de3-4cac-4e6b-9bbd-3bb718c0920e1696485422044-Desktop-Main-Banner_02.jpg" alt = ""/>
+            </div>     
+            <div>
+                <img className='item' src = "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2023/10/4/6401663d-5123-45d9-944f-1bce5ce39ecb1696438281384-Sponsorship-strip.jpg" alt = ""/>
+            </div> 
+            <div>
+                <img className = "item" src = "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2023/10/4/094f8577-046a-43cd-a8a1-fec47b56419e1696437688078-Coupon-Corner.jpg" alt = ""/>
+            </div>
+            <div className='item1'>
+                <img className = "item3"src = "https://assets.myntassets.com/f_webp,w_490,c_limit,fl_progressive,dpr_2.0/assets/images/2023/10/4/679723f6-90d2-4bf9-aca5-105b621d999d1696437898130-MYNTRA100--2-.jpg" alt = ""/>
+                <img className="item3" src = "https://assets.myntassets.com/f_webp,w_490,c_limit,fl_progressive,dpr_2.0/assets/images/2023/10/4/2ce0c964-b149-462b-bdf8-059af6175db51696437898119-MYNTRA200--2-.jpg"alt =""/>
+            </div>
+            <div>
+                <img className = "item"src = "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2023/10/4/d441b128-d26f-4533-a7f4-1f9679decfeb1696437688115-OMG-Deals.jpg" alt = ""/>
+            </div>
+            <div>
+                <img className = "item" src = {img1} alt = ""/>
+            </div>
+            <div>
+                <img className = "item" src = {img2} alt = ""/>
+            </div>
+            <div>
+                <img className = "item" src = {img3} alt = ""/>
+            </div>
+            <div>
+                <img className = "item" src = "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2023/10/4/673c184f-4970-4534-a3ee-af23ebf95c731696438098430-App-Install-Banner--3-.jpg" alt = ""/>
+            </div>
+         </div>
         </>
     )
 }
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,10 +1,93 @@
 import React from "react";
 
+
 function Login(){
+    const [input,setInput] = React.useState({
+        number:""
+
+    })
+    const [inputstore,setInputStore] = React.useState(0)
+
+    const handleChange = (e)=>{
+        const {name,value} = e.target
+        setInput({...input,[name]:value})
+    }
+
+    React.useEffect(()=>{
+        fetch(`http://localhost:3000/loginData`)
+        .then((res)=>res.json())
+        .then((json)=>setInputStore(json))
+      },[])
+
+    const handleSubmit = (e)=>{
+        console.log(e.target,"target")
+      e.preventDefault()
+      setInputStore(input.length)
+
+        fetch(`http://localhost:3000/loginData`,{
+            method:"POST",
+            body:JSON.stringify(input),
+            headers:{
+                "Content-type":"application/json"
+            }
+
+        })
+        .then((res)=>res.json())
+        .then((json)=>console.log(json,"effect"))
+      }
+    
+     
+    //   if(input.length === 10){
+    //     alert("Login successfully")
+    //   }
+    //   else{
+    //     alert("Choose your Number")
+    //   }
+    
+    const {number} = input;
     return (
         <>
-        This is login
+        <div className="loginContainer">
+            {/* This is the login page */}
+            <div className="loginPart">
+            <div>
+             <img className="item" src = "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/2023/2/7/c2be095d-a4fb-4981-bdad-9d69ea189da31675792659902-offer-banner-500-600x240-code-_-MYNTRA200.jpg" alt =  ""/>
+            </div>
+            <div>
+                <h2>Login or Signup</h2>
+            </div>
+            <div>
+                {/* <input type = "text" placeholder="Enter your mobile number"/> */}
+
+            </div>
+        <form onSubmit = {handleSubmit}>
+
+            {/* <div class="form-row"> */}
+    <div >
+      <input className = "inputForms" style = {{width:"60%",padding:"2%"}} 
+      type="number" 
+      class="form-control" 
+      placeholder="Enter your mobile number"
+      name = "number"
+      value = {number}
+      onChange = {handleChange}/>
+    </div>
+            <div>
+                <p className="Para">
+                By continuing, I agree to the <span className="loginPart2"><b>Terms of Use & Private Policy</b> </span>
+                  </p>     
+            </div>
+            <div>
+                <button className="continueButton" type = "submit" value = "submit">Continue</button>
+            </div>
+      </form>
+
+            <div>
+                <p className="Para">Have troubled logging in ?<span className="loginPart2"><b>Get Help</b></span></p>
+            </div>
+            </div>
+        </div>
         </>
     )
-}
+    }
 export {Login}
