6b2ef763c8148e90e50389bdf7b73b66ac616ef3
Madhavi Rabbagundu <madhavirabbagundu@gmail.com>
2023-10-24 23:36:59 +0530

    today i am working on cart page need to try to display similar images in that but its not done . i am working on that

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -4,21 +4,41 @@ import { useParams } from 'react-router-dom'
 function Cart(){
     const {item} = useParams();
     console.log(item)
-    const [selectedData,setSelectedData] = React.useState({})
+    const [selectedData,setSelectedData] = React.useState([])
+    // const [cartImages,setcartImage] = React.useState([])
 
     React.useEffect(()=>{
  fetch(`http://localhost:3000/men/${item}`)
  .then((res)=>res.json())
  .then((json)=>{setSelectedData(json)})
     },[item])
-    console.log(selectedData,"cart")
+    var data = JSON.stringify(selectedData.subImages)
+    console.log(typeof(data))
+    // console.log(typeof(data),typeof(selectedData),"images")
+     
     return(
         <>
         <div>
+             <div className='cartContainer'>
        
-                <img src = {selectedData.image} alt = ""/>
-                
+                <img className = "cartImage" src = {selectedData.subImages} alt = ""/>
+             {/* {
+                selectedData.subImages.map(item=>(
+                    <div>
+                        <img src = {item.img} alt = ""/>
+                    </div>
+                ))
+             } */}
+             <img src = {data}/>
+                </div>
         </div>
+        {/* <div>
+            {data.map(item => {
+                <div>
+                    <img className = "cartImage" src = {item} alt = ""/>
+                    </div>
+})}
+        </div> */}
         </>
     )
 }
